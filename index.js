496a3610a5434fbc152605353b40056a20d8c5bb
ROBIND KUMAR <kumarrobind386@gmail.com>
2024-10-24 21:30:30 +0530

    api to load college and branch list

--- index.js
+++ index.js
@@ -21,6 +21,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Routes: get all colleges
+app.get("/api/college", (req, res) => {
+  const query = "SELECT code, name FROM college";
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(rows);
+  });
+});
+
+// Routes: get all branches
+app.get("/api/branch", (req, res) => {
+  const query = "SELECT code, name FROM branch";
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(rows);
+  });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
